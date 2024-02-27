a0258e253e7122cc532c496d04643b6d3cb80db0
abdulhaseebj <abdulhaseeb4326@gmail.com>
2024-02-27 16:41:58 +0500

    crud completed

--- index.js
+++ index.js
@@ -1,18 +1,57 @@
 const express = require('express');
 require('dotenv').config()
-
+var cors = require('cors')
 const app = express()
+app.use(cors())
+app.use(express.json());
 
+const arr = []
 
-app.get('/', (req, res) => {
-  res.send('Hello home')
+//get all users
+app.get('/api/v1/users', (req, res) => {
+  res.send(arr)
 })
 
-app.get('/store', (req, res) => {
-  res.send('Hello store')
+// add user
+app.post('/api/v1/users', (req, res) => {
+  const { title } = req.body
+  arr.push({
+    title: title,
+    id: Date.now(),
+  })
+  res.send('user added to successfully')
 })
+
+//delete user
+app.delete('/api/v1/users/:id', (req, res) => {
+  const { id } = req.params
+  const index = arr.findIndex((user) => user.id === Number(id))
+  if (index === -1) {
+    res.send('user not found')
+    return
+  }
+  arr.splice(index, 1)
+  res.send('user deleted')
+})
+
+// edit user
+app.put('/api/v1/users/:id', (req, res) => {
+  const {title} = req.body
+  const { id } = req.params
+  const index = arr.findIndex((user) => user.id === Number(id))
+  if (index === -1) {
+    res.send('user not found')
+    return
+  }
+  arr[index].title = title
+  res.send('user edited successfuly')
+})
+
+
+
+
 
 
 app.listen(process.env.PORT, () => {
-    console.log(`Example app listening on port ${process.env.PORT}`)
+  console.log(`Example app listening on port ${process.env.PORT}`)
 })
